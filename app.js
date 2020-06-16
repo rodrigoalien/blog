a667d3caceb1ade0604a18412c3fcd5187168b06
rodrigoalien <rodrigodacunhaalmeida@gmail.com>
2020-06-16 07:51:55 -0300

    Layout and Title post.

--- app.js
+++ app.js
@@ -15,8 +15,36 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
+app.get("/", function(req, res){
+    
+    res.render("home", {
+        homeContent: homeStartingContent
+    });
+});
 
+app.get("/about", function(req, res){
 
+    res.render("about", {
+        aboutDescription : aboutContent
+    });
+});
+
+app.get("/contact", function(req, res){
+    
+    res.render("contact", {
+        contactText: contactContent
+    });
+});
+
+app.get("/compose", function(req, res){
+
+    res.render("compose");
+});
+
+app.post("/compose", function(req, res){
+    
+    console.log(req.body.postTitle);
+});
 
 
 
